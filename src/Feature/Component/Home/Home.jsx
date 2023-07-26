93f3d880882d4def01ea289a1b48ba6b6079a1be
Ohm <hwatcharawit05@gmail.com>
2023-07-26 14:02:17 +0700

    update: landing page

--- src/Feature/Component/Home/Home.jsx
+++ src/Feature/Component/Home/Home.jsx
@@ -14,6 +14,7 @@ function Home() {
     //         console.error('Error occurred:', error.message);
     //     }
     // }
+    
     useEffect(() => {
         async function fetchmyapi() {
             try {
